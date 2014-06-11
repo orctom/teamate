5776a9774f4fa06f2622877f7e431a96228868b6
orctom <orctom@gmail.com>
2014-06-11 15:33:45 +0800

    updated template

--- app.js
+++ app.js
@@ -6,12 +6,37 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var passport = require('passport');
+var LdapStrategy = require('passport-ldapauth').Strategy;
+
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/teamate');
 var rest = require('restler');
 
 var app = express();
+
+// ldap
+var OPTS = {
+    server: {
+        url: 'ldap://uschnahub01.na.odcorp.net:3268/',
+        adminDn: 'CN=SVC-JENKINS-DEV,OU=System_OU,DC=na,DC=odcorp,DC=net',
+        adminPassword: 'May@3014',
+        searchBase: 'DC=na,DC=odcorp,DC=net',
+        searchFilter: '(&amp;(objectclass=user)(sAMAccountName={0}))'
+    }
+}
+
+// passport
+passport.use(new LdapStrategy(OPTS));
+app.use(passport.initialize());
+app.post('/login', passport.authenticate('ldapauth', {
+    session: false
+}), function(req, res) {
+    res.send({
+        status: 'ok'
+    });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +50,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
-
+// routes
 var routes = require('./routes');
 var users = require('./routes/user');
 
@@ -33,7 +58,7 @@ app.get('/', routes.index);
 app.get('/users', users.list(db));
 app.get('/user', users.lookup(rest));
 
-
+// sheduler
 var schedule = require('node-schedule');
 schedule.scheduleJob('* * * * 1-5', function() {
     console.log('The answer to life, the universe, and everything!');
@@ -70,4 +95,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.listen('3000', function() {
+    console.log('Server listening on port ' + '3000');
+});
